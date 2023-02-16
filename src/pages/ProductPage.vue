d23e886b0040035e1cb926ae58d4de94f72adf5a
audran2 <audran.massacry@next-u.fr>
2023-02-16 22:59:02 +0100

    page produit (ModalPrice, description produit...)

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -1,57 +1,127 @@
 <template>
-    <div>
-        <NavBar />
-        <div class="product-page">
-            <b-carousel>
-                <b-carousel-slide v-for="photo in product.photos" :key="photo.url">
-                    <img :src="photo.url" alt="Product Photo">
-                </b-carousel-slide>
-            </b-carousel>
-            <div class="product-details">
-                <h2>{{ product.name }}</h2>
-                <p>{{ product.description }}</p>
-                <p>{{ product.brand }}</p>
-                <modal-add-to-cart :product="product" />
-            </div>
-        </div>
+  <div class="product-page">
+    <NavBar />
+    <router-link
+      class="right"
+      :to="{
+        name: 'home',
+      }"
+    >
+      ← Retour
+    </router-link>
+    <h2 class="title">{{ product.title }}</h2>
+    <div class="images">
+      <img
+        v-for="(image, index) in product.images"
+        :key="index"
+        :src="image"
+        alt="product"
+      />
     </div>
+    <div class="product-desc">
+      <div class="description">
+        <p>Ref n°{{ product.id }}</p>
+        <p>Nom: {{ product.title }}</p>
+        <p>Marque : {{ product.brand }}</p>
+        <p>Description: {{ product.description }}</p>
+      </div>
+      <ModalPrice
+        class="modal-price"
+        :unitPrice="product.price"
+        :id="product.id"
+        :title="product.title"
+        :url_thumbnail="product.thumbnail"
+        :quantity="product.quantity"
+        @update:quantity="updateQuantity"
+      />
+    </div>
+  </div>
 </template>
-
 <script>
-import NavBar from '../components/NavBar.vue'
-import ModalAddToCart from '../components/ModalAddToCart.vue'
-import axios from 'axios';
-
+import ModalPrice from "../components/ModalPrice.vue";
+import NavBar from "../components/NavBar.vue";
+import axios from "axios";
 export default {
-    components: {
-        NavBar,
-        ModalAddToCart,
+  name: "ProductPage",
+  components: {
+    NavBar,
+    ModalPrice,
+  },
+  data() {
+    return {
+      product: {},
+    };
+  },
+  mounted() {
+    const productId = this.$route.params.productId;
+    axios
+      .get(`https://dummyjson.com/products/${productId}`)
+      .then((response) => {
+        this.product = response.data;
+      });
+  },
+  methods: {
+    updateQuantity(quantity) {
+      this.product.quantity = quantity;
+      this.$emit("update:quantity", { id: this.product.id, quantity });
     },
-    data () {
-        return {
-            product: {}
-        }
-    },
-    created () {
-        this.fetchProduct()
-    },
-    methods: {
-        async fetchProduct () {
-            const {data} = await axios.get('https://dumyjson.com/api/product/${this.$route.params.id}')
-            this.product = data
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
-    .product-page {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    .product-details {
-        margin-top: 1rem;
-        text-align: center;
-    }
+.product-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.product-page a {
+  margin-left: 20px;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: black;
+  text-decoration: none;
+}
+.right {
+  position: relative;
+  padding-top: 10px;
+  left: -800px;
+}
+.product-desc {
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.description {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.modal-price {
+  width: 500px;
+}
+
+h2 {
+  margin: 20px 0;
+  text-align: center;
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+.product-page img {
+  width: 50%;
+  height: auto;
+  margin: 20px 0;
+  display: inline-block;
+}
+
+p {
+  margin: 10px 0;
+  text-align: center;
+  font-size: 1.5rem;
+}
+
 </style>
