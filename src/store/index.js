50cc0d8337bce929104e05e503dac57264ba4c4f
audran2 <audran.massacry@next-u.fr>
2023-02-16 23:01:16 +0100

    store (fonction d'ajout, de modification et de suppression d'un produit)

--- src/store/index.js
+++ src/store/index.js
@@ -1,42 +1,67 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import persistancePlugin from "./plugins/persistancePlugin.js";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {
-        cart: []
+  state: {
+    cart: [],
+  },
+  mutations: {
+    addProduct(state, product) {
+      state.cart.push(product);
     },
-    mutations: {
-        addProductToCart(state, product) {
-            state.cart.push(product)
-        },
-        updateProductQuantity(state, {product, quantity}) {
-            const cartProduct = state.cart.find(item => item.ref === product.ref)
-            cartProduct.quantity = quantity
-        },
-        removeProductFromCart(state, product) {
-            const index = state.cart.findIndex(item => item.ref === product.ref)
-            state.cart.splice(index, 1)
-        }
+    updateProduct(state, payload) {
+      const { id, quantity } = payload;
+      const productIndex = state.cart.findIndex((item) => item.id === id);
+      if (!state.cart[productIndex]) return;
+
+      state.cart[productIndex].quantity = quantity;
+      state.cart[productIndex].subtotal =
+        quantity * state.cart[productIndex].price;
     },
-    actions: {
-        addProduct({commit}, product) {
-            commit('addProductToCart', product)
-        },
-        updateProduct({commit}, {product, quantity}) {
-            commit('updateProductQuantity', {product, quantity})
-        },
-        removeProduct({commit}, product) {
-            commit('removeProductFromCart', product)
-        }
+    removeProduct(state, productId) {
+      const productIndex = state.cart.findIndex(
+        (product) => product.id === productId
+      );
+      state.cart.splice(productIndex, 1);
     },
-    getters: {
-        cartProducts(state) {
-            return state.cart
-        },
-        cartTotal(state) {
-            return state.cart.reduce((acc, item) => acc + item.quantity * item.price, 0)
-        }
-    }
-})
+    setCart(state, cart) {
+      state.cart = cart;
+    },
+  },
+
+  actions: {
+    addProduct({ commit, state }, product) {
+      if (product.quantity <= 0) return;
+      const existingProduct = state.cart.find(
+        (cartProduct) => cartProduct.id === product.id
+      );
+      if (existingProduct) {
+        const updatedProduct = {
+          ...existingProduct,
+          quantity: existingProduct.quantity + product.quantity,
+        };
+        updatedProduct.subtotal =
+          updatedProduct.quantity * existingProduct.price;
+        commit("updateProduct", updatedProduct);
+      } else {
+        product.subtotal = product.quantity * product.price;
+        commit("addProduct", product);
+      }
+    },
+    updateProduct({ commit }, payload) {
+      commit("updateProduct", payload);
+    },
+    removeProduct({ commit }, productId) {
+      commit("removeProduct", productId);
+    },
+  },
+  getters: {
+    cartProductsCount(state) {
+      return state.cart.length;
+    },
+  },
+  plugins: [persistancePlugin],
+});
