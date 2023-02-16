46e6d888c8f6b63be20bb75602966f42df875dcd
audran2 <audran.massacry@next-u.fr>
2023-02-16 23:03:38 +0100

    changement page App

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,9 @@
 <template>
   <div>
-    <NavBar />
-    <div class="container my-5">
-      <div class="row">
-        <ProductCard 
-          v-for="product in paginatedProducts"
-          :key="product.id"
-          :product="product"
-        />
-      </div>
-    </div>
-    <PaginationComponent />
+    <router-view></router-view>
   </div>
 </template>
 
-<script>
-import NavBar from './components/NavBar.vue';
-import ProductCard from './components/ProductCard.vue';
-import PaginationComponent from './components/PaginationComponent.vue';
-import axios from 'axios';
-
-export default {
-  components: {
-    NavBar,
-    ProductCard,
-    PaginationComponent,
-  },
-  data() {
-    return {
-      products: [],
-      currentPage: 1,
-      productsPerPage: 10,
-    };
-  },
-  computed: {
-    paginatedProducts() {
-      const start = (this.currentPage - 1) * this.productsPerPage;
-      const end = start + this.productsPerPage;
-      return this.products.slice(start, end);
-    },
-  },
-  mounted() {
-    axios
-      .get('https://dumyjson.com/docs/todos')
-      .then(response => {
-        this.products = response.data;
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  },
-};
-</script>
+<style>
+@import "~bulma/css/bulma.css";
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import App from './App.vue'
-
-Vue.config.productionTip = false
+import store from './store'
+import router from './router';
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  store,
+  router,
+  render: h => h(App)
+}).$mount('#app');
